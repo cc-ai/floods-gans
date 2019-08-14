0e22aa88951bd9c1869fad883dff8a5227a69af7
Victor Schmidt <9283470+vict0rsch@users.noreply.github.com>
2019-08-14 17:36:49 -0400

    Create get_image_from_gps.py

---
+++ various_scripts/get_image_from_gps.py
@@ -0,0 +1,22 @@
+
+import google_streetview.api as gsv_api
+import google_streetview.helpers as gsv_helpers
+
+
+def get_image_from_gps(coords):
+    params = {
+        "size": "512x512",
+        "location": "45.5307147,-73.6157818",
+        "pitch": "10",
+        "radius": "1000",
+        "key": "ASK MIKE OR VICTOR",
+        "source": "outdoor",
+        "fov": "120",
+    }
+    api_list = gsv_helpers.api_list(params)
+    results = gsv_api.results(api_list)
+    return results
+    
+# then: results.download_links(".") 
+# change "." to another path 
+# if you don't want to download images in the current working directory
