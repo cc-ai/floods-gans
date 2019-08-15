478cd4cd374047a2075b1e8f1a578d4787f4ad17
Victor Schmidt <9283470+vict0rsch@users.noreply.github.com>
2019-08-15 00:39:05 -0400

    docs

--- various_scripts/get_image_from_gps.py
+++ various_scripts/get_image_from_gps.py
@@ -3,7 +3,19 @@ import google_streetview.api as gsv_api
 import google_streetview.helpers as gsv_helpers
 
 
-def get_image_from_gps(coords):
+def get_image_from_gps(coords: str) -> Result:
+    """
+    Get a result object containing the link to an street view image
+    in the vicinity of the specified lat and long coordinates
+    
+    use as results.download_links(".") 
+    change "." to another path not to download in cwd
+
+    ex: 
+    res = get_image_from_gps("45.5307147,-73.6157818")
+    res.download_links("~/Downloads") 
+    
+    """
     params = {
         "size": "512x512",
         "location": coords,
@@ -16,7 +28,3 @@ def get_image_from_gps(coords):
     api_list = gsv_helpers.api_list(params)
     results = gsv_api.results(api_list)
     return results
-    
-# then: results.download_links(".") 
-# change "." to another path 
-# if you don't want to download images in the current working directory
